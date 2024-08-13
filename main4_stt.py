540c73215f01b4c9c1213e5119ff5e04e77b2aff
descartes3104 <d.h.3104@gmail.com>
2024-08-13 15:50:16 +0900

    Update main4_stt.py

--- main4_stt.py
+++ main4_stt.py
@@ -1,8 +1,5 @@
-import streamlit as st
-import speech_recognition as sr
-import sounddevice as sd
-import numpy as np
-import wavio
+import streamlit as st  # フロントエンドを扱うstreamlitの機能をインポート
+import speech_recognition as sr  # 音声認識の機能をインポート
 
 # 言語選択と、APIが認識する言語の変換リストを作成
 set_language_list = {
@@ -13,44 +10,56 @@ set_language_list = {
 # デフォルトの言語を設定
 set_language = "日本語"
 
-# 音声を録音する関数
-def record_audio(duration, fs, filename):
-    print("Recording...")
-    audio = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype='int16')
-    sd.wait()
-    wavio.write(filename, audio, fs, sampwidth=2)
-    print("Recording complete")
+# 音声ファイルと音声認識の言語を引数に音声認識をする
+def file_speech_to_text(audio_file, set_language):
+    recognizer = sr.Recognizer()
+    # 音声ファイルを読み込み
+    with sr.AudioFile(audio_file) as source:
+        audio = recognizer.record(source)  # sr.Recognizer().record(開いた音声ファイル)で認識準備
+
+    try:
+        text = recognizer.recognize_google(audio, language=set_language_list[set_language])  # sr.Recognizer().recognize_google(音声データ,言語)で音声認識して、textに代入
+    except sr.UnknownValueError:
+        text = "音声認識に失敗しました"
+    except sr.RequestError as e:
+        text = f"音声認識サービスに接続できませんでした; {e}"
+    return text  # 認識した文字を返す
 
 # 音声認識の言語を引数に音声認識をする
-def mic_speech_to_text(set_language, filename):
+def mic_speech_to_text(set_language):
     recognizer = sr.Recognizer()
-    with sr.AudioFile(filename) as source:
-        audio = recognizer.record(source)
-        try:
-            text = recognizer.recognize_google(audio, language=set_language_list[set_language])
-        except:
-            text = "音声認識に失敗しました"
-    return text
+    # マイク入力を音声ファイルとして読み込み
+    with sr.Microphone() as source:
+        audio = recognizer.listen(source)  # sr.Recognizer().listen(マイク入力)で認識準備
 
-st.title("文字起こしアプリ")
-st.write("音声認識する言語を選んでください。")
-set_language = st.selectbox("音声認識する言語を選んでください。", set_language_list.keys())
-current_language_state = st.empty()
-current_language_state.write("選択中の言語:" + set_language)
-file_upload = st.file_uploader("ここに音声認識したファイルをアップロードしてください。", type=["wav"])
+    try:
+        text = recognizer.recognize_google(audio, language=set_language_list[set_language])  # sr.Recognizer().recognize_google(音声データ,言語)で音声認識して、textに代入
+    except sr.UnknownValueError:
+        text = "音声認識に失敗しました"
+    except sr.RequestError as e:
+        text = f"音声認識サービスに接続できませんでした; {e}"
+    return text  # 認識した文字を返す
 
+st.title("文字起こしアプリ")  # タイトル
+st.write("音声認識する言語を選んでください。")  # 案内を表示
+set_language = st.selectbox("音声認識する言語を選んでください。", set_language_list.keys())  # 音声認識に使う言語を選択肢として表示
+current_language_state = st.empty()  # 選択肢を表示するための箱を準備
+current_language_state.write("選択中の言語:" + set_language)  # 選択肢を表示するための箱に選択した言語を表示
+file_upload = st.file_uploader("ここに音声認識したファイルをアップロードしてください。", type=["wav"])  # アップローダーを設定し、wavファイルだけ許可する設定にする
+
+# ファイルアップロードされた場合、file_uploadがNoneではなくなる
 if file_upload is not None:
-    st.write("音声認識結果:")
-    result_text = file_speech_to_text(file_upload, set_language)
-    st.write(result_text)
-    st.audio(file_upload)
+    st.write("音声認識結果:")  # 案内表示
+    result_text = file_speech_to_text(file_upload, set_language)  # アップロードされたファイルと選択した言語を元に音声認識開始
+    st.write(result_text)  # メソッドから返ってきた値を表示
+    st.audio(file_upload)  # アップロードした音声をきける形で表示
 
-st.write("マイクでの音声認識はこちらのボタンから")
+st.write("マイクでの音声認識はこちらのボタンから")  # 案内表示
 
+# ボタンが押されたら実行される
 if st.button("音声認識開始"):
-    state = st.empty()
-    state.write("音声認識中")
-    record_audio(5, 44100, "output.wav")  # 5秒間録音
-    result_text = mic_speech_to_text(set_language, "output.wav")
-    state.write("音声認識結果:")
-    st.write(result_text)
+    state = st.empty()  # マイク録音中を示す為の箱を準備
+    state.write("音声認識中")  # 箱に案内表示書き込み
+    result_text = mic_speech_to_text(set_language)  # 選択した言語を元に音声認識開始
+    state.write("音声認識結果:")  # 案内表示に変更
+    st.write(result_text)  # メソッドから返ってきた値を表示
