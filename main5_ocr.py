fa11eb8b6553bca79428dfd1be992fd2613660db
descartes3104 <d.h.3104@gmail.com>
2024-08-14 15:08:14 +0900

    Update main5_ocr.py

--- main5_ocr.py
+++ main5_ocr.py
@@ -3,11 +3,12 @@ import streamlit as st
 from PIL import Image
 import pyocr
 import platform
+import pytesseract
 
 if platform.system() == "Windows":
-    pyocr.tesseract.TESSERACT_CMD = r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+    pytesseract.pytesseract.tesseract_cmd = r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 else:
-    pyocr.tesseract.TESSERACT_CMD = r"/usr/local/bin/tesseract"
+    pytesseract.pytesseract.tesseract_cmd = r"/usr/local/bin/tesseract"
 
 set_language_list = {
     "日本語":"jpn",
@@ -21,9 +22,12 @@ file_upload = st.file_uploader("ここに文字が書かれた画像ファイル
 if (file_upload !=None):
     st.image(file_upload)
     engines = pyocr.get_available_tools()
-    engine = engines[0]
-    txt = engine.image_to_string(Image.open(file_upload), set_language_list[set_language])
-    st.write(txt)
+    if not engines:
+        st.error("OCRエンジンが見つかりませんでした。Tesseractが正しくインストールされているか確認してください。")
+    else:
+        engine = engines[0]
+        txt = engine.image_to_string(Image.open(file_upload), set_language_list[set_language])
+        st.write(txt)
     
     st.write("感情分析の結果")
     from asari.api import Sonar
